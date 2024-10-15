7abdff08f2e8787e00e4f6fd929fc50aa8f08eb9
hussein-qaraa <husseinqaraa05@gmail.com>
2024-10-15 15:12:28 -0400

    Updated repo

---
+++ src/main/java/com/bptn/course/teachbackcode/TeachBackCode.java
@@ -0,0 +1,21 @@
+package com.bptn.course.teachbackcode;
+import java.util.*;
+
+
+public class TeachBackCode {
+	
+	public static void main(String[] args) {
+	LinkedList<String> colour = new LinkedList<String>();
+	
+	colour.add("Green"); 
+	colour.add("Blue");
+	colour.add("Red");
+	
+	System.out.println(colour);
+	
+	System.out.println(colour.get(0));
+	System.out.println(colour.get(1));
+	System.out.println(colour.get(2));
+	
+	}
+}
